202d48fab95a723155604b8216b103353976b862
sanbob <saintsanbob@gmail.com>
2026-09-19 20:35:52 +0800

    refactor(db): remove unnecessary quoting from table and column names

--- db/dump-data.sql
+++ db/dump-data.sql
@@ -1,7 +1,7 @@
 -- DML Data Manipulation Language
 
 INSERT INTO
-    "ranks" ("title")
+    ranks (title)
 VALUES ('Рядовой'),
     ('Сержант'),
     ('Лейтенант'),
@@ -9,7 +9,7 @@ VALUES ('Рядовой'),
 ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO
-    "equipment_types" ("id", "title", "abbreviation")
+    equipment_types (id, title, abbreviation)
 VALUES (
         1,
         'Десантный метео комплект',
@@ -19,7 +19,7 @@ VALUES (
 ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO
-    "users" ("name", "surname", "rank_id")
+    users (name, surname, rank_id)
 VALUES ('Иван', 'Иванов', 1),
     ('Петр', 'Петров', 2),
     ('Алексей', 'Сидоров', 3),
@@ -27,32 +27,32 @@ VALUES ('Иван', 'Иванов', 1),
 ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO
-    "parameters" (
-        "equipment_type_id",
-        "station_height",
-        "temperature",
-        "pressure",
-        "wind_direction",
-        "wind_speed"
+    parameters (
+        equipment_type_id,
+        station_height,
+        temperature,
+        pressure,
+        wind_direction,
+        wind_speed
     )
 VALUES (1, 101, 26.4, 762, 55, 8),
     (1, 105, 22.1, 734, 12, 3)
 ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO
-    "parameters" (
-        "equipment_type_id",
-        "station_height",
-        "temperature",
-        "pressure",
-        "wind_direction",
-        "bullet_drift_distance"
+    parameters (
+        equipment_type_id,
+        station_height,
+        temperature,
+        pressure,
+        wind_direction,
+        bullet_drift_distance
     )
 VALUES (2, 160, -10.0, 670, 12, 72)
 ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO
-    "measurements" ("user_id", "parameters_id")
+    measurements (user_id, parameters_id)
 VALUES (1, 1),
     (3, 2),
     (4, 3)
